3f9ec43de6c33b095b3451fa79dc81d24897d27a
N1VC3UJ779DDQ3Z\Administrator <tangminglei@yunzhangfang.com>
2020-11-09 10:24:04 +0800

    gengxin

--- ServiceTestingVip1/src/test/java/com/tk/wework/contact/MemberTest.java
+++ ServiceTestingVip1/src/test/java/com/tk/wework/contact/MemberTest.java
@@ -3,7 +3,10 @@ package com.tk.wework.contact;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,18 +16,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberTest {
 
     public static Member member;
+
     @BeforeAll
-    void setUp() {
+    static void setUp() {
         member = new Member();
     }
 
-    @Test
-    void create() {
-        Map<String,Object> map = new HashMap<String,Object>();
-        map.put("userid",member.random);
-        map.put("name","TestName"+member.random);
-        map.put("department",[5]);
-        map.put("email",member.random+"@qq.com");
-        member.create(map).then().body("errcode",equalTo(0));
+    @ParameterizedTest
+    @ValueSource(strings = {"Test", "Haha"})
+    void create(String name) {
+        String nameNew = name + member.random;
+        String random = String.valueOf(System.currentTimeMillis()).substring(5,13);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userid", nameNew);
+        map.put("name", nameNew);
+        map.put("email", random + "@qq.com");
+        map.put("mobile","186"+ random);
+        map.put("department", Arrays.asList(1, 2));
+        member.create(map).then().body("errcode", equalTo(0));
     }
 }
